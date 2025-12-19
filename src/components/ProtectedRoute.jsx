7ce4237dddada84eaf16adafd4f26d912350eada
Utkarsh Phalphale <phalphaleutkarsh@gmail.com>
2025-12-19 17:19:17 +0530

    Delete src directory

--- src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom';
-
-const ProtectedRoute = ({user,children}) => {
-  return user ? children : <Navigate to ="/auth"/>;
-  
-};
-
-export default ProtectedRoute
